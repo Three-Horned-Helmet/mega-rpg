a1b85d302978584862decf2811a3bdccc0db2f3e
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-06-01 16:17:40 +0200

    minor updates

--- models/User.js
+++ models/User.js
@@ -1,43 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	username: String,
-	userId: String,
-	banned: Boolean,
-	coolDowns: {
-		hunt: {
-			type: Number,
-			default:0,
-		},
-	},
-	health: {
-		type: Number,
-		default: 100,
-	},
-	attack: {
-		type: Number,
-		default: 5,
-	},
-	defense: {
-		type: Number,
-		default: 3,
-	},
-	inventory: {
-		smallHealPotion: {
-			type: Number,
-			default: 1,
-		},
-	},
-	currentExp: {
-		type: Number,
-		default: 1,
-	},
-	expToNextLevel: {
-		type: Number,
-		default: 100,
-	},
+  username: String,
+  userId: String,
+  banned: Boolean,
+  coolDowns: {
+    hunt: {
+      type: Date,
+      default: 0,
+    },
+  },
+  hero: {
+    health: {
+      type: Number,
+      default: 100,
+    },
+    attack: {
+      type: Number,
+      default: 5,
+    },
+    defense: {
+      type: Number,
+      default: 3,
+    },
+    inventory: {
+      smallHealPotion: {
+        type: Number,
+        default: 1,
+      },
+    },
+    currentExp: {
+      type: Number,
+      default: 1,
+    },
+    expToNextLevel: {
+      type: Number,
+      default: 100,
+    },
+  },
 });
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
