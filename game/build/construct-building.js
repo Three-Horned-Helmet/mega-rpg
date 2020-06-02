3c6c6075e23333d129e99d7024126a4a6b868dab
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-02 20:49:02 +0200

    made a nicer channel message

--- game/build/construct-building.js
+++ game/build/construct-building.js
@@ -24,7 +24,7 @@ const checkIfBuildIsPossible = (user, building, coordinates) => {
 	if(!buildingCost) return { response: false, message:"You have already reached max level" };
 
 	for(const resource in buildingCost) {
-		if(!(user.resources[resource] > buildingCost[resource])) {
+		if(!(user.resources[resource] > buildingCost[resource]) && resource !== "level") {
 			return {
 				response: false,
 				message: `You are missing ${user.resources[resource] ? buildingCost[resource] - user.resources[resource] : buildingCost[resource]} of ${resource}` };
@@ -40,6 +40,8 @@ const constructBuilding = async (user, building, coordinates) => {
 	if(!responseBuild.response) return responseBuild;
 
 	const { buildingCost } = responseBuild;
+
+	// Creates the new building object that is stored in the database
 	const newBuilding = {
 		name: building.name,
 		position: coordinates,
