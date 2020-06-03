2dfd875b42fe5e64fb6a7195b457bf7a0efaef53
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-06-03 22:43:32 +0200

    profile find logic fix

--- commands/profile.js
+++ commands/profile.js
@@ -1,5 +1,5 @@
 const { prettifyUser } = require("../game/profile");
-const User = require("./../models/User");
+const User = require("../models/User");
 
 module.exports = {
   name: "profile",
@@ -9,18 +9,21 @@ module.exports = {
 
 
     let dbUser;
+    let userIdFromArgs = null
     if (args.length) {
-      // if the user wants the profile of someone other than him or herself
-      const userIdFromArgs = args[0].match(/\d+/)[0];
+      userIdFromArgs = args[0].match(/\d+/)
+    }
+    if (userIdFromArgs) {
       try {
-        dbUser = await User.findOne({ "account.userId": userIdFromArgs }); // retrieves the user mentioned in as argument eg: !profile @Ignore
-      } catch (error) {
-        console.error("error: ", error);
-        dbUser = user; // failsafe. If user tries to get the profile from someone who does not exist
+        dbUser = await User.findOne({ "account.userId": userIdFromArgs[0] }); // retrieves the user mentioned in as argument eg: !profile @Ignore
+      }
+      catch (err) {
+        console.error('error: ', err)
       }
     } else {
       dbUser = user;
     }
+
     const prettifiedUser = prettifyUser(message, dbUser);
     message.channel.send(prettifiedUser);
   },
