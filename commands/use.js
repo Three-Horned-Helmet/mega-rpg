2a7ac2f28e2405e6b20bf50c91d3d6ad1d1482b8
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-06 22:28:52 +0200

    added consumable healing potions

--- commands/use.js
+++ commands/use.js
@@ -1,6 +1,24 @@
+const { consumablesObject } = require("../game/_CONSTS/consumables-object");
+
 module.exports = {
 	name: "use",
 	description: "Use any consumables like healing potions.",
-	execute(message, args, user) {
+	async execute(message, args, user) {
+		if(args.length === 0) {
+			return message.channel.send("What item do you want to use");
+		}
+
+		args = args.map(a => a.charAt(0).toUpperCase() + a.slice(1));
+
+		const item = consumablesObject[args.join(" ")];
+
+		if(item) {
+			const response = await item.execute(user, item);
+			message.channel.send(response);
+		}
+		else {
+			message.channel.send(`There are no consumables called ${args.join(" ")}`);
+		}
+
 	},
 };
--- game/_CONSTS/consumables-object.js
+++ game/_CONSTS/consumables-object.js
@@ -1,28 +1,27 @@
-const consumableObject = {
+const consumablesObject = {
 	["Small Heal Potion"]: {
 		name: "Small Heal Potion",
 		healingValue: 100,
-		execute: async (user) =>{
-			console.log("HEALING VALUE ", this.healingValue);
+		execute: async (user, item) =>{
 			// Does the user have the item
-			if(!(user.hero.inventory[this.name] > 0)) return false;
+			if(!(user.hero.inventory[item.name] > 0)) return `You have no ${item.name} in your inventory`;
 
-			const updatedUser = await user.healHero(this.healingValue, this.name);
-			return updatedUser.hero.currentHealth;
+			const updatedUser = await user.healHero(item.healingValue, item.name);
+			return `You drank a ${item.name} and your hero has ${updatedUser.hero.currentHealth}/${updatedUser.hero.health} hp left`;
 		},
 	},
 	["Large Heal Potion"]: {
 		name: "Large Heal Potion",
 		healingValue: 300,
-		execute: async (user) =>{
-			console.log("HEALING VALUE ", this.healingValue);
+		execute: async (user, item) =>{
+			console.log("HEALING VALUE ", item.healingValue);
 			// Does the user have the item
-			if(!(user.hero.inventory[this.name] > 0)) return false;
+			if(!(user.hero.inventory[item.name] > 0)) return `You have no ${item.name} in your inventory`;
 
-			const updatedUser = await user.healHero(this.healingValue, this.name);
-			return updatedUser.hero.currentHealth;
+			const updatedUser = await user.healHero(item.healingValue, item.name);
+			return `You drank a ${item.name} and your hero has ${updatedUser.hero.currentHealth}/${updatedUser.hero.health} hp left`;
 		},
 	},
 };
 
-module.exports = { consumableObject };
+module.exports = { consumablesObject };
