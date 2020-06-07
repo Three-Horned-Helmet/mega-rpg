24a1a3d542d585a79b42c2a57603f058e5b5d202
HaakonA <haakonaaa@stud.ntnu.no>
2020-06-07 16:44:26 +0200

    eslint activated

--- commands/fish.js
+++ commands/fish.js
@@ -1,12 +1,12 @@
 const { handleFish } = require("../game/fish");
 
 module.exports = {
-    name: "fish",
-    aliases: ["f"],
-	description: "Let's the player fish",
-	async execute(message, args, user) {
-		const result = await handleFish(user);
-		console.log(result, 'fishing result')
-        return message.channel.send(result);
-	},
+  name: "fish",
+  aliases: ["f"],
+  description: "Let's the player fish",
+  async execute(message, args, user) {
+    const result = await handleFish(user);
+    console.log(result, "fishing result");
+    return message.channel.send(result);
+  },
 };
--- game/fish/index.js
+++ game/fish/index.js
@@ -1,46 +1,49 @@
-const {onCooldown} = require("../_CONSTS/cooldowns")
+const { onCooldown } = require("../_CONSTS/cooldowns");
 
 const handleFish = async (user) => {
-    console.log('handlefish function!')
-    const {explored} = user.world.locations["Grassy Plains"]
-    const {currentLocation} = user.world
-    const onCooldownInfo = onCooldown('fish', user)
+    console.log("handlefish function!");
+    const { explored } = user.world.locations["Grassy Plains"];
+    const { currentLocation } = user.world;
+    const onCooldownInfo = onCooldown("fish", user);
 
-    const now = new Date()
-    if (onCooldownInfo.response){
-        return onCooldownInfo.embed
+    const now = new Date();
+    if (onCooldownInfo.response) {
+        return onCooldownInfo.embed;
     }
-    if(!explored.includes("River")){
-        return `You have not explored any rivers yet in ${currentLocation}`
+    if (!explored.includes("River")) {
+        return `You have not explored any rivers yet in ${currentLocation}`;
     }
 
 
-    const randomNumber = Math.random()
-    const chance = 0.5
+    const randomNumber = Math.random();
+    const chance = 0.5;
 
-    let goldResult=0
-    let fish = ['Cod','Trout','Swordfish']
-    let fishResult = ''
+    let goldResult = 0;
+    const fish = ["Cod", "Trout", "Swordfish"];
+    let fishResult = "";
 
-    if (randomNumber>chance){
-        fishResult = fish[Math.floor(Math.random()*fish.length)] // 'Swordfish'
-        let multiplier = fish.indexOf(fishResult) // 0,1,2
-        goldResult = Math.floor(multiplier * 20 + (Math.random()*10)) // 0-50
+    if (randomNumber > chance) {
+        // 'Swordfish'
+        fishResult = fish[Math.floor(Math.random() * fish.length)];
+        // 0,1,2
+        const multiplier = fish.indexOf(fishResult);
+        // 0-50
+        goldResult = Math.floor(multiplier * 20 + (Math.random() * 10));
 
-        return `you caught a ${fishResult} and sold it for ${goldResult} gold`
+        return `you caught a ${fishResult} and sold it for ${goldResult} gold`;
     }
-    user.handleFishResult(goldResult,now)
+    user.handleFishResult(goldResult, now);
 
-    return generateFailFishSentence()
-}
+    return generateFailFishSentence();
+};
 
-const generateFailFishSentence = ()=>{
+const generateFailFishSentence = () => {
     const sentences = [
         "You fished for hours with no luck",
         "You lost your bait while fishing",
         "You caught an old boot",
-        "You put your fishing rod down and took a swim instead"]
-        return sentences[Math.floor(Math.random()*sentences.length)]
-}
+        "You put your fishing rod down and took a swim instead"];
+    return sentences[Math.floor(Math.random() * sentences.length)];
+};
 
-module.exports={handleFish}
+module.exports = { handleFish };
