0a78b921353b0ef804eedd0fbde053186143192d
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-07 16:13:40 +0200

    added pve full combat

--- combat/combat.js
+++ combat/combat.js
@@ -1,7 +1,21 @@
 const calculateStats = require("./calculate-stats");
 
-// const pveFullArmy = (user, npc) => {
-// };
+const pveFullArmy = async (user, npc) => {
+	const { totalStats: userStats } = calculateStats(user);
+	const combatModifier = (1 - Math.random() / 2);
+	const userHp = userStats.health * combatModifier;
+	const userAt = userStats.attack * combatModifier;
+	const { health: oppHp, attack: oppAt } = npc.stats;
+
+	const losses = (userHp + userAt) - (oppHp + oppAt);
+	const win = losses > 0 ? true : false;
+	let lossPercentage = ((userHp + userAt) - (oppHp + oppAt)) / (userHp + userAt);
+	lossPercentage = lossPercentage < 0 ? 0 : lossPercentage;
+
+	await user.unitLoss(lossPercentage);
+
+	return { win, lossPercentage, combatModifier };
+};
 
 // user vs opponent duel with full army (units + hero), returns an object with the winner and loser
 const pvpFullArmy = async (user, opp) => {
@@ -36,4 +50,4 @@ const pvpFullArmy = async (user, opp) => {
 };
 
 
-module.exports = { pvpFullArmy };
+module.exports = { pveFullArmy, pvpFullArmy };
