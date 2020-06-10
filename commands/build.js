af42ad93f76a03439858f26fd764c44d8f1c244f
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-10 11:24:07 +0200

    added a show available buildings embed

--- commands/build.js
+++ commands/build.js
@@ -1,11 +1,12 @@
 const buildingsObject = require("../game/build/buildings-object");
 const constructBuilding = require("../game/build/construct-building");
+const availableBuilds = require("../game/build/show-available-builds");
 
 module.exports = {
 	name: "build",
 	description: "Build a structure.",
 	execute(message, args, user) {
-		if (args.length === 0) return message.channel.send("You need an argument");
+		if (args.length === 0) return message.channel.send(availableBuilds(user));
 		const building = buildingsObject[args.slice(0, args.length - 1).join(" ")];
 		const coordinates = args[args.length - 1].split(".").map(cord => parseInt(cord));
 
