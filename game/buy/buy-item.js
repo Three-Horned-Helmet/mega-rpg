ec1b06b86cd9f1b960aaa97a333b08dc43920535
Tormod Flesjø <32653149+Ojself@users.noreply.github.com>
2020-06-10 13:40:27 +0200

    Update buy-item.js

--- game/buy/buy-item.js
+++ game/buy/buy-item.js
@@ -8,7 +8,6 @@ const handleBuyCommand = async (args, user) =>{
 	}
 
 	const joinedArg = args.map(a => a.charAt(0).toUpperCase() + a.slice(1).toLowerCase()).join(" ");
-	console.log(joinedArg, "OINED");
 	const item = consumeObj[joinedArg];
 
 	const message = await buyItem(user, item);
