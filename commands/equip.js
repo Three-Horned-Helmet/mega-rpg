7d332075f542248844eeedfdb55550bb9aa57b73
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-11 17:27:54 +0200

    added equip shortcuts

--- commands/equip.js
+++ commands/equip.js
@@ -1,10 +1,11 @@
 const equipItem = require("../game/equip/equip-item");
 const showEquipmentEmbed = require("../game/equip/show-available-equipment");
-
+const allItemShortcuts = require("../game/items/all-items-shortcut");
 
 module.exports = {
 	name: "equip",
 	description: "Equip items for your hero",
+	shortcuts: allItemShortcuts,
 	execute(message, args, user) {
 		if(args.length === 0) return message.channel.send(showEquipmentEmbed(user));
 
