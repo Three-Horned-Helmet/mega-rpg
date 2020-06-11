fcfa768a34dd7ea3d10a640fc2313afd816f748a
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-11 11:22:05 +0200

    added display crafts and shortcuts

--- commands/craft.js
+++ commands/craft.js
@@ -1,10 +1,13 @@
 const craftItem = require("../game/craft/craft-item");
+const displayCrafts = require("../game/craft/show-available-crafts");
+const allItemShortcuts = require("../game/items/all-items-shortcut");
 
 module.exports = {
 	name: "craft",
 	description: "crafts items",
+	shortcuts: allItemShortcuts,
 	execute(message, args, user) {
-		if(args.length === 0) return message.channel.send("You need to apply arguments");
+		if(args.length === 0) return message.channel.send(displayCrafts(user));
 
 		const item = args.slice(0, args.length - 1).join(" ");
 		const amount = Math.floor(args[args.length - 1]);
