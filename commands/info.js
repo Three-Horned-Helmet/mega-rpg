47df7cdbb9f805e5ccec377dd5bf8acd38fef62c
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-12 11:15:35 +0200

    added a display for all our info

---
+++ commands/info.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+module.exports = {
+	name: "info",
+	description: "Gives an overview of the different commands and their functionalities (big chunk of text)",
+	async execute(message) {
+        const data = fs.readFileSync("info.txt", "utf8");
+        for(let i = 0; i < data.length; i += 2000) {
+            const toSend = data.substring(i, Math.min(data.length, i + 2000));
+            message.author.send(toSend);
+        }
+	},
+};
