d75659838f9511538c033e5dec0f0b809dc57faa
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-14 15:33:08 +0200

    added more info

--- commands/use.js
+++ commands/use.js
@@ -2,14 +2,14 @@ const consumablesObject = require("../game/use/consumables-object");
 
 module.exports = {
 	name: "use",
-	description: "Use any consumables like healing potions from your inventory (!army).",
+	description: "Use any consumables like healing potions from your inventory. Type `!army` to see your inventory. Try `!use small heal potion` to use a healing potion. You need a shop to buy more consumables.",
 	shortcuts: {
 		shp: "small heal potion",
 		lhp: "large heal potion",
 	},
 	async execute(message, args, user) {
 		if(args.length === 0) {
-			return message.channel.send(`<@${message.author.id}>: What item do you want to use`);
+			return message.channel.send(`<@${message.author.id}>: What consumable do you want to use? Try \`!use small heal potion\``);
 		}
 
 		args = args.map(a => a.charAt(0).toUpperCase() + a.slice(1).toLowerCase());
