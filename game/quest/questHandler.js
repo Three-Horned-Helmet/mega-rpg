5c88d02bdec17b794bbdecb3f0da07894c69fb21
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-16 12:55:49 +0200

    added several available quests handling

--- game/quest/questHandler.js
+++ game/quest/questHandler.js
@@ -1,6 +1,6 @@
 const allQuests = require("./all-quests");
 
-const questHandler = async (user) => {
+const questHandler = async (user, questName) => {
     // If there are no quests available
     if(user.quests.length === 0) return "You currently have no available quests. You may get new ones when you explore new areas or obtain specific items or building levels";
 
@@ -17,6 +17,11 @@ const questHandler = async (user) => {
         if(!questResponse) return showQuestDescription(quest);
         else return showQuestRewards(quest);
     }
+
+    // If the user has several quests available, but no args is given
+    if(!questName) {
+        return showAvailableQuests(user);
+    }
 };
 
 // Change this to an embed
@@ -28,6 +33,17 @@ const showQuestDescription = (quest) => {
 // Change this to an embed
 const showQuestRewards = (quest) => {
     const msg = `Congratulations you have completed __${quest.name}__! ${quest.winDescription || ""}\n\n__Rewards:__\n${quest.reward}`;
+    return msg;
+};
+
+// Change this to an embed
+const showAvailableQuests = (user) => {
+    let msg = `You have ${user.quests.length} quests available:\n`;
+    user.quests.forEach(q => {
+        msg += `${q.name}\n`;
+    });
+
+    msg += `\nChoose what quest you want to show (e.g. \`!quest ${user.quests[0].name}\``;
     return msg;
 };
 
