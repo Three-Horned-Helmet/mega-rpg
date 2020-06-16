c2762bf814e3f469a69c43ee2261de8735944d59
Tormod Flesjø <32653149+Ojself@users.noreply.github.com>
2020-06-16 10:01:28 +0200

    Update questHandler.js
    Unused variabel

--- game/quest/questHandler.js
+++ game/quest/questHandler.js
@@ -1,6 +1,6 @@
 const allQuests = require("./all-quests");
 
-const questHandler = async (user, questName) => {
+const questHandler = async (user) => {
     // If there are no quests available
     if(user.quests.length === 0) return "You currently have no available quests. You may get new ones when you explore new areas or obtain specific items or building levels";
 
