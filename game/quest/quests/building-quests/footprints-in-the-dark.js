253ca42f8dbfe4b3456f6144ccd2b4f4b2cf164c
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-22 20:12:35 +0200

    added more quests

--- game/quest/quests/building-quests/footprints-in-the-dark.js
+++ game/quest/quests/building-quests/footprints-in-the-dark.js
@@ -66,7 +66,7 @@ module.exports = {
         found: "While following the trail you hear a twig cracking",
         notFound: "You lost track of the footsteps and got lost. Maybe it is better to go back and try again?",
         description: "The footprints lead away from the Lumbermill, and you decide to follow them alone to make sure the lumberjacks were telling the truth and they had nothing to do with destruction. You bring your equipment and set out into the forest.",
-        winDescription: "You turn to the noise, quickly drawing your weapon! Walking a step toward the source of the sound you feel a sharp pain at the back of the head. The world turns black as you lose consciousness.",
+        winDescription: "You turn to the noise, quickly drawing your weapon! Walking a step toward the source of the sound you feel a sharp pain at the back of the head. The world turns black as you lose consciousness.\n**A new quest is available**",
         objective: "Follow the footsteps into the **Forest**",
         reward: "Oak wood: 30\n",
         questKeySequence: ["Building Quests", "findingSaboteurs"],
@@ -84,22 +84,16 @@ module.exports = {
 
        // Get reward
        await user.gainManyResources({
-           gold: 70,
+           "oak wood": 30,
        });
 
        // Add next quest
-    //    const newQuest = {
-    //        name: "Confronting the Bandits",
-    //        started: false,
-    //        questKeySequence: ["Grassy Plains", "confrontingBandits"],
-    //        pve: [{
-    //            name: "Confront Bandits",
-    //            completed: false,
-    //            chance: 1,
-    //            unique: true,
-    //        }],
-    //    };
-    //    user.addNewQuest(newQuest);
+       const newQuest = {
+           name: "The Wood Elves",
+           started: false,
+           questKeySequence: ["Building Quests", "woodElves"],
+       };
+       user.addNewQuest(newQuest);
        user.removeQuest(this.name);
 
        await user.save();
@@ -107,9 +101,164 @@ module.exports = {
             return true;
         },
     },
+
+    woodElves: {
+        name: "The Wood Elves",
+        author: "Sindre Heldrup",
+        description: "You wake up by a fire, feeling your hands tied together, but otherwise feeling fine apart from a dull pain at the back of your head. You squint your eyes due to the light emanating from the fire, looking around you. A bunch of what appears to be elves are clustered around you, looking at you with curious eyes.\n\nYou think: 'What’s the matter with them, have they never seen a handsome man before.'\n\nA voice rises above the murmur from the crowd: 'Bring forth the human'.\n\nYou are brought in front of a tall and slender elf sitting on a wooden throne elevated by a wooden platform. His hair is silver, his eyes shining with wisdom and age, and his body covered with a clothing of green and brown. \n\nHe asks you: 'Why were you out in our wood at the middle of the night Human?'\n\n**Three answers spring to your mind:**\n```diff\nChoice 1:\n- 'Whatever, I’ll do what I want.' Uttering the words you also decide to flip him off to prove how much of a badass you really are. (!quest %questIndex% choice 1)\n\nChoice 2:\n- 'I just awoke in the night and thought it was a fantastic idea to rise from bed and take a quick stroll in the wood. You know, just for fun, me being such a dedicated nature lover.' (!quest %questIndex% choice 2)\n\nChoice 3:\n- 'My lumbermill was raided and I was following the tracks when I was ambushed.' (!quest %questIndex% choice 3)\n```",
+        winDescription: "",
+        winChoice1: "The elf on the throne looks at you with disgust written all over his face. \n\nIn a loud and clear voice he says: 'Off with his head'.\n\nYou decide to do what is heroic and burp while the axe is swinging toward your neck. You think: I do not think anyone has died while burping before, I’m such a bada...\n\n*- Your Lumbermill's level decreased back to 0*\n*- Your hero died*",
+        winChoice2: "The elf on the throne look at you with raised eyebrows.\n\nHe says: 'Do you really think I will believe a human would take a walk through the woods by themselves for the beauty of the forest? Answer me, but do not lie to me again Human. This is your one and only warning.'\n**A new quest is available**",
+        winChoice3: "*\\*The elven lord looks at you with a serious face.\\**\n\nHe says: 'Now that is honesty if I’ve ever seen it. I appreciate that. Because of that I will provide you with the answers you seek.'\n**A new quest is available**",
+        questKeySequence: ["Building Quests", "woodElves"],
+
+        // Returns false if the quest description is shown, or true if the quest is being completed
+        execute: async function(user, choice) {
+            const questResponse = await questHelper(user, this.name);
+            if(!questResponse || !choice) {
+                return false;
+            }
+
+            const choiceNumber = parseInt(choice[1]);
+            // Only 2 answers possible
+            if(!(choiceNumber <= 3 && choiceNumber >= 1)) return false;
+
+            user.removeQuest(this.name);
+
+            if(choiceNumber === 1) {
+                await user.heroHpLoss(1);
+                user.decreaseBuildingLevel("lumbermill", 1, 1);
+                this.winDescription = this.winChoice1;
+            }
+            else if (choiceNumber === 2) {
+                this.winDescription = this.winChoice2;
+
+                const newQuest = {
+                    name: "Lies are not Tollerated",
+                    started: false,
+                    questKeySequence: ["Building Quests", "liesNotTollerated"],
+                };
+
+                user.addNewQuest(newQuest);
+            }
+            else if (choiceNumber === 3) {
+                // this.winDescription = this.winChoice2;
+
+                // const newQuest = {
+                //     name: "Lies are not Tollerated",
+                //     started: false,
+                //     questKeySequence: ["Building Quests", "liesNotTollerated"],
+                // };
+
+                // user.addNewQuest(newQuest);
+            }
+
+            await user.save();
+
+            return true;
+        },
+    },
+    liesNotTollerated: {
+        name: "Lies are not Tollerated",
+        author: "Sindre Heldrup",
+        description: "*\\*The elf on the throne look at you with raised eyebrows.\\**\n\nHe says: 'Do you really think I will believe a human would take a walk through the woods by themselves for the beauty of the forest? Answer me, but do not lie to me again Human. This is your one and only warning.'\n\n**You think of two possible answers:**\n```diff\nChoice 1:\n- 'No, really, I do love the forest with all my heart and taking random walks in it during the night is my life’s true calling. You know, the woods are basically my second home!' (!quest %questIndex% choice 1)\n\nChoice 2:\n- 'My Lumbermill was raided and I were following the tracks when I was ambushed.' (!quest %questIndex% choice 2)\n```",
+        winDescription: "",
+        winChoice1: "You buckle down on your answer: 'No, really, I do love the forest with all my heart and taking random walks in it during the night is my life’s true calling. You know, the woods are basi...'\n\n*\\*The elven lord points to his mouth and you are gagged before being able to finish your love ballad of the forest.\\**\n\nThe elven lord: 'Off with his head.'\n\n*- Your Lumbermill's level decreased back to 0*\n*- Your hero died*",
+        winChoice2: "*\\*The elven lord looks at you with a serious face.\\**\n\nHe says: 'Now that is honesty if I’ve ever seen it. I appreciate that. Because of that I will provide you with the answers you seek.'\n**A new quest is available**",
+        questKeySequence: ["Building Quests", "liesNotTollerated"],
+
+        // Returns false if the quest description is shown, or true if the quest is being completed
+        execute: async function(user, choice) {
+            const questResponse = await questHelper(user, this.name);
+            if(!questResponse || !choice) {
+                return false;
+            }
+
+            const choiceNumber = parseInt(choice[1]);
+            // Only 2 answers possible
+            if(!(choiceNumber <= 2 && choiceNumber >= 1)) return false;
+
+            user.removeQuest(this.name);
+
+            if(choiceNumber === 1) {
+                await user.heroHpLoss(1);
+                user.decreaseBuildingLevel("lumbermill", 1, 1);
+                this.winDescription = this.winChoice1;
+            }
+            else if (choiceNumber === 2) {
+                // this.winDescription = this.winChoice2;
+
+                // const newQuest = {
+                //     name: "Finding the Saboteurs",
+                //     started: false,
+                //     questKeySequence: ["Building Quests", "findingSaboteurs"],
+                //     pve: [{
+                //         name: "Forest",
+                //         completed: false,
+                //         chance: 0.25,
+                //     }],
+                // };
+
+                // user.addNewQuest(newQuest);
+            }
+
+            await user.save();
+
+            return true;
+        },
+    },
+
+    newStart: {
+        name: "A New Start",
+        author: "Sindre Heldrup",
+        description: "You look at the Elven Lord expectantly hoping he is about to give you the answers of how to strike it off with that hot bartender you’ve been staring down every night for the past month.\n\nHe continues: 'It was some of my hunters, returning from a hunt but noticing a section of the forest missing, chopped down by your lumberjacks...'\n\nYou look at him with disappointment written all over your face as you realize he won’t provide you with a brilliant strategy to strike it off with the bartender. \n\nHe says: 'Yes, I can see the disappointment in your face, I realize a great deal of materials and equipment were destroyed by my elves...'\n\n*\\*You quickly nod, feeling relief he can’t read your mind.\\**\n\nHe says: 'We do not wish for bad blood between our two people, and therefore I __offer__ to help you replace the equipment lost and rebuild what we destroyed. However, I will not punish the hunters, nor can I guarantee it never happens again. But this time we will make up for it, as a sign of good faith. Give your answer by tomorrow!'\n\n**You return to your village and think about your options, identifying three possible routes to take:**\n```diff\nChoice 1:\n- Do nothing and go back to sleep. Damn, but you feel very tired already. If you catch some good hours before the days works you can possibly go to the bar in the evening and stare down the bartender some more. Tempting. (!quest %questIndex% choice 1)\n\nChoice 2:\n- Rouse your warriors, those elves must be punished! (!quest %questIndex% choice 2)\n\nChoice 3:\n- You return the following night to the designated area, meeting the Elven Lord and accept his offer to help you rebuild and replace the equipment. (!quest %questIndex% choice 3)\n```",
+        winDescription: "",
+        winChoice1: "You buckle down on your answer: 'No, really, I do love the forest with all my heart and taking random walks in it during the night is my life’s true calling. You know, the woods are basi...'\n\n*\\*The elven lord points to his mouth and you are gagged before being able to finish your love ballad of the forest.\\**\n\nThe elven lord: 'Off with his head.'\n\n*- Your Lumbermill's level decreased back to 0*\n*- Your hero died*",
+        winChoice2: "*\\*The elven lord looks at you with a serious face.\\**\n\nHe says: 'Now that is honesty if I’ve ever seen it. I appreciate that. Because of that I will provide you with the answers you seek.'\n**A new quest is available**",
+        questKeySequence: ["Building Quests", "liesNotTollerated"],
+
+        // Returns false if the quest description is shown, or true if the quest is being completed
+        execute: async function(user, choice) {
+            const questResponse = await questHelper(user, this.name);
+            if(!questResponse || !choice) {
+                return false;
+            }
+
+            const choiceNumber = parseInt(choice[1]);
+            // Only 2 answers possible
+            if(!(choiceNumber <= 2 && choiceNumber >= 1)) return false;
+
+            user.removeQuest(this.name);
+
+            if(choiceNumber === 1) {
+                await user.heroHpLoss(1);
+                user.decreaseBuildingLevel("lumbermill", 1, 1);
+                this.winDescription = this.winChoice1;
+            }
+            else if (choiceNumber === 2) {
+                // this.winDescription = this.winChoice2;
+
+                // const newQuest = {
+                //     name: "Finding the Saboteurs",
+                //     started: false,
+                //     questKeySequence: ["Building Quests", "findingSaboteurs"],
+                //     pve: [{
+                //         name: "Forest",
+                //         completed: false,
+                //         chance: 0.25,
+                //     }],
+                // };
+
+                // user.addNewQuest(newQuest);
+            }
+
+            await user.save();
+
+            return true;
+        },
+    },
 };
 
-// "You wake up by a fire, feeling your hands tied together, but otherwise feeling fine apart from a dull pain at the back of your head. You squint your eyes due to the light emanating from the fire, looking around you. A bunch of what appears to be elves are clustered around you, looking at you with curious eyes.\n\You think: What’s the matter with them, have they never seen a handsome man before.\n\nA voice rises above the murmur from the crowd: 'Bring forth the human'.\n\nYou are brought in front of a tall and slender elf sitting on a wooden throne elevated by a wooden platform. His hair is silver, his eyes shining with wisdom and age, and his body covered with a clothing of green and brown. \n\nHe asks you: 'Why were you out in our wood at the middle of the night Human?'\n\nThree answers spring to your mind:"
 
 const questHelper = async (user, questName) => {
     const quest = user.quests.find(q => q.name === questName);
