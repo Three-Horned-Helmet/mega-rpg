5536acba9684d50171703f0fde1a87e9c4bdc9fc
Markus Heldrup <markusheldrup@hotmail.com>
2020-06-26 15:09:00 +0200

    added quest key to the mansion

--- game/quest/quests/grassy-plains/entering-the-mansion.js
+++ game/quest/quests/grassy-plains/entering-the-mansion.js
@@ -57,10 +57,10 @@ module.exports = {
      // ENTERING THE MANSION
      keyToMansion: {
         name: "The Key to the Mansion",
-        description: "",
-        objective: "",
+        description: "'You know... I once knew Julius Banethus, the person now known as the Bandit King.He used to be an honorable man, who cared about his people! Julius and his wife Migreth used to come by me quite frequently back in the days to get keys crafted, and we ended up becoming quite good friends!'\n\n*\\*A smile appeared accross his face, but when he continued his face turned dark\\**\n\n'This was when Migreth was still alive. She was expecting a child and was going to conceive it out in the Forest where they had a magnificent Cabin together. No one knows what really happened there, but the Cabin got set on fire and Migreth nor the child made it out alive. Poor Julius was devastated and after a while he could not stand it anymore and left the country for a long time. When he came back...'\n\n*\\*He paused and the look on his face was that of a broken man\\**\n\n'Oh well, you told me you wanted that key made, huh? I need a few things for that to happen. First of all I need some Obsidian bars to craft the key, but only Morith the Blacksmith knows the craft, he is an older man, but still very sharp and daring. Unfortuneatly, no one has seen him for quite a while so you better get prepared for a little searching\nSecondly, I need the key Mold. This one will also be a bit tricky, but you can talk with Grethel, she may know how to get it. Good luck!'\n\n*\\*You thank the Blacksmith and leaves the blacksmith\\**\n**Two new quests are available**",
+        objective: "Get the Key Mold and the Obsidian bars",
         reward: "Key to the Mansion: 1",
-        winDescription: "",
+        winDescription: "'Perfect! This is exactly what I need to create the key! Feel free to sit and wait while I carft it.'\n\n*\\*You sat down by an old table in the corner of the house and watched for a few hours while Ahred crafted the key\\**'There we go, this should do it!'\n\n*\\*Ahred hands you the key\\**'Careful, it is still a bit hot!'\n\n*\\*You take the key deeply impressed. It is really a beautiful key!\\**\n\n'Thank you so much for your help, Ahred! If I can ever repay you, please let me know!'\n\n'Oh, don't worry about that! Just remember, I don't know what business you have with the Bandit King, but don't forget that he was once a very king and honorable man, and I believe that man is still there inside of him somewhere!'\n\n*\\*You shake Ahreds hand in gratitude and leave the locksmith with a shiny freshly-made obsidian key. Now it's time to see if it actually works!\\**",
         questKeySequence: ["Grassy Plains", "keyToMansion"],
 
         // Returns false if the quest description is shown, or true if the quest is being completed
@@ -70,7 +70,7 @@ module.exports = {
 
 
             // Has the user completed the two quests
-            if(!(user.completedQuests.includes("Melting Obsidian")) && !(user.completedQuests.includes("Mirgreth's Mission"))) return false;
+            if(!(user.completedQuests.includes("Melting Obsidian")) && !(user.completedQuests.includes("Grethel's Mission"))) return false;
 
             // Get reward
             // await user.gainManyResources({
