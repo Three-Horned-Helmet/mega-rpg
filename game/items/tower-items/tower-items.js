27087d0b0fc907cf7a1bd8709cf27c6e71a26a21
Markus Heldrup <markusheldrup@hotmail.com>
2020-08-07 13:13:50 +0200

    fixed drops

--- game/items/tower-items/tower-items.js
+++ game/items/tower-items/tower-items.js
@@ -49,174 +49,213 @@ module.exports = {
 		},
 		towerItem: true,
 	},
-
-	// HELMETS
-	"Spiked Helmet": {
-		name: "Spiked Helmet",
-		typeSequence: ["army", "armory", "helmet"],
+	"Ice Lord Mace": {
+		name: "Ice Lord Mace",
+		typeSequence: ["army", "armory", "weapon"],
 		requirement: {
 			building: "armorer",
 			level: 999,
 		},
 		stats: {
-			attack: 10,
-			health: 20,
+			attack: 50,
+			health: 15,
 		},
 		towerItem: true,
 	},
-	"Black Knight Helmet": {
-		name: "Black Knight Helmet",
-		typeSequence: ["army", "armory", "helmet"],
+	"Necromancers Staff": {
+		name: "Necromancers Staff",
+		typeSequence: ["army", "armory", "weapon"],
 		requirement: {
 			building: "armorer",
 			level: 999,
 		},
 		stats: {
-			health: 35,
-		},
-		towerItem: true,
-	},
-	"Assassins Hood": {
-		name: "Assassins Hood",
-		typeSequence: ["army", "armory", "helmet"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			attack: 20,
-			health: 20,
-		},
-		towerItem: true,
-	},
-	"Skull Helmet": {
-		name: "Skull Helmet",
-		typeSequence: ["army", "armory", "helmet"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
+			attack: 30,
 			health: 40,
 		},
 		towerItem: true,
 	},
-
-	// CHEST
-	"Beastal Breastplate": {
-		name: "Beastal Breastplate",
-		typeSequence: ["army", "armory", "chest"],
+	"Hollow Death Bringer": {
+		name: "Hollow Death Bringer",
+		typeSequence: ["army", "armory", "weapon"],
 		requirement: {
 			building: "armorer",
 			level: 999,
 		},
 		stats: {
-			health: 35,
-		},
-		towerItem: true,
-	},
-	"Hellforged Chainmail": {
-		name: "Hellforged Chainmail",
-		typeSequence: ["army", "armory", "chest"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			attack: 5,
-			health: 35,
-		},
-		towerItem: true,
-	},
-	"Ancient Warlords Platebody": {
-		name: "Ancient Warlords Platebody",
-		typeSequence: ["army", "armory", "chest"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			health: 40,
-		},
-		towerItem: true,
-	},
-	"Moriths Heavy Platebody": {
-		name: "Moriths Heavy Platebody",
-		typeSequence: ["army", "armory", "chest"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			health: 50,
-		},
-		towerItem: true,
-	},
-	"Retribution Chainbody": {
-		name: "Retribution Chainbody",
-		typeSequence: ["army", "armory", "chest"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			attack: 25,
-			health: 25,
-		},
-		towerItem: true,
-	},
-
-	// Leggings
-	"Rubinite Leggings": {
-		name: "Rubinite Leggings",
-		typeSequence: ["army", "armory", "legging"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			health: 25,
-		},
-		towerItem: true,
-	},
-	"Revenants Platelegs": {
-		name: "Revenants Platelegs",
-		typeSequence: ["army", "armory", "legging"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			attack: 25,
+			attack: 70,
 			health: 5,
 		},
 		towerItem: true,
 	},
-	"Mystic Robes": {
-		name: "Mystic Robes",
-		typeSequence: ["army", "armory", "legging"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			attack: 10,
-			health: 25,
-		},
-		towerItem: true,
-	},
-	"Heavens Platelegs": {
-		name: "Heavens Platelegs",
-		typeSequence: ["army", "armory", "legging"],
-		requirement: {
-			building: "armorer",
-			level: 999,
-		},
-		stats: {
-			health: 40,
-		},
-		towerItem: true,
-	},
+
+	// // HELMETS
+	// "Spiked Helmet": {
+	// 	name: "Spiked Helmet",
+	// 	typeSequence: ["army", "armory", "helmet"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		attack: 10,
+	// 		health: 20,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Black Knight Helmet": {
+	// 	name: "Black Knight Helmet",
+	// 	typeSequence: ["army", "armory", "helmet"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 35,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Assassins Hood": {
+	// 	name: "Assassins Hood",
+	// 	typeSequence: ["army", "armory", "helmet"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		attack: 20,
+	// 		health: 20,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Skull Helmet": {
+	// 	name: "Skull Helmet",
+	// 	typeSequence: ["army", "armory", "helmet"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 40,
+	// 	},
+	// 	towerItem: true,
+	// },
+
+	// // CHEST
+	// "Beastal Breastplate": {
+	// 	name: "Beastal Breastplate",
+	// 	typeSequence: ["army", "armory", "chest"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 35,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Hellforged Chainmail": {
+	// 	name: "Hellforged Chainmail",
+	// 	typeSequence: ["army", "armory", "chest"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		attack: 5,
+	// 		health: 35,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Ancient Warlords Platebody": {
+	// 	name: "Ancient Warlords Platebody",
+	// 	typeSequence: ["army", "armory", "chest"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 40,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Moriths Heavy Platebody": {
+	// 	name: "Moriths Heavy Platebody",
+	// 	typeSequence: ["army", "armory", "chest"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 50,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Retribution Chainbody": {
+	// 	name: "Retribution Chainbody",
+	// 	typeSequence: ["army", "armory", "chest"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		attack: 25,
+	// 		health: 25,
+	// 	},
+	// 	towerItem: true,
+	// },
+
+	// // Leggings
+	// "Rubinite Leggings": {
+	// 	name: "Rubinite Leggings",
+	// 	typeSequence: ["army", "armory", "legging"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 25,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Revenants Platelegs": {
+	// 	name: "Revenants Platelegs",
+	// 	typeSequence: ["army", "armory", "legging"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		attack: 25,
+	// 		health: 5,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Mystic Robes": {
+	// 	name: "Mystic Robes",
+	// 	typeSequence: ["army", "armory", "legging"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		attack: 10,
+	// 		health: 25,
+	// 	},
+	// 	towerItem: true,
+	// },
+	// "Heavens Platelegs": {
+	// 	name: "Heavens Platelegs",
+	// 	typeSequence: ["army", "armory", "legging"],
+	// 	requirement: {
+	// 		building: "armorer",
+	// 		level: 999,
+	// 	},
+	// 	stats: {
+	// 		health: 40,
+	// 	},
+	// 	towerItem: true,
+	// },
 };
 
 // Heavens platelegs of the bear (5)
