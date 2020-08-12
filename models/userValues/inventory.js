42aa6329d1dd354eaa0792ec444b38fc3be51381
Markus Heldrup <markusheldrup@hotmail.com>
2020-08-12 16:45:51 +0200

    done

---
+++ models/userValues/inventory.js
@@ -0,0 +1,36 @@
+const inventory = {
+	["Small Healing Potion"]: {
+		type: Number,
+		default: 5,
+	},
+	["Large Healing Potion"]: {
+		type: Number,
+		default: 0,
+	},
+	["Enourmous Healing Potion"]: {
+		type: Number,
+		default: 0,
+	},
+	["Quality Healing Potion"]: {
+		type: Number,
+		default: 0,
+	},
+	["Mega Healing Potion"]: {
+		type: Number,
+		default: 0,
+	},
+	["Ultra Healing Potion"]: {
+		type: Number,
+		default: 0,
+	},
+	["Small Healing Salve"]: {
+		type: Number,
+		default: 0,
+	},
+	["Large Healing Salve"]: {
+		type: Number,
+		default: 0,
+	},
+};
+
+module.exports = { inventory };
