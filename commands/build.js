1a9c42b115468e250e375e4c80da2d7c5d01c447
fenrew <fenrew@gmail.com>
2021-07-29 21:31:39 +0200

    destroy command now destroys the lowest leveled building

--- commands/build.js
+++ commands/build.js
@@ -3,44 +3,55 @@ const constructBuilding = require("../game/build/construct-building");
 const availableBuilds = require("../game/build/show-available-builds");
 
 module.exports = {
-	name: "build",
-	description: "Build or upgrade structures used for creating an army. Try: `!build mine` followed by `!grid` to get started.",
-	usage: "!build mine 0.1",
-	shortcuts: {
-		ba: "barracks",
-		ar: "archery",
-		fa: "farm",
-		mi: "mine",
-		lu: "lumbermill",
-		fo: "forge",
-		bl: "blacksmith",
-		arm: "armorer",
-		se: "senate",
-		sh: "shop",
-	},
-	execute(message, args, user) {
-		if (args.length === 0) return message.channel.send(availableBuilds(user));
-		const building = buildingsObject[args.slice(0, args.length - 1).join(" ")] || buildingsObject[args.slice(0, args.length).join(" ")];
+  name: "build",
+  description:
+    "Build or upgrade structures used for creating an army. Try: `!build mine` followed by `!grid` to get started.",
+  usage: "!build mine 0.1",
+  shortcuts: {
+    ba: "barracks",
+    ar: "archery",
+    fa: "farm",
+    mi: "mine",
+    lu: "lumbermill",
+    fo: "forge",
+    bl: "blacksmith",
+    arm: "armorer",
+    se: "senate",
+    sh: "shop",
+  },
+  execute(message, args, user) {
+    if (args.length === 0) return message.channel.send(availableBuilds(user));
+    const building = Object.values(buildingsObject).find((building) =>
+      args.includes(building.name)
+    );
 
-		// Remove the user.empire.find stuff to make it possible to build several of the same building
-		// || (building && user.empire.find(b => b.name === building.name))
-		if(args[args.length - 1] === "-u" && building) {
-			const usersBuildings = user.empire.filter(b => b.name === building.name).sort((a, b) => a.level - b.level);
-			if(usersBuildings.length > 0) {
-				args[args.length - 1] = usersBuildings[0].position.join(".");
-			}
-		}
+    // Remove the user.empire.find stuff to make it possible to build several of the same building
+    // || (building && user.empire.find(b => b.name === building.name))
+    if (args.includes("-u") && building) {
+      const gridCords = args.find((el) => el.match(/\d+\.\d+/g));
+      const usersBuildings = user.empire
+        .filter((b) =>
+          gridCords
+            ? b.name === building.name &&
+              b.position[0] === parseInt(gridCords.split(".")[0]) &&
+              b.position[1] === parseInt(gridCords.split(".")[1])
+            : b.name === building.name
+        )
+        .sort((a, b) => a.level - b.level);
+      if (usersBuildings.length > 0) {
+        args[args.length - 1] = usersBuildings[0].position.join(".");
+      }
+    }
 
-		const coordinates = args[args.length - 1].split(".").map(cord => parseInt(cord));
+    const coordinates = args[args.length - 1]
+      .split(".")
+      .map((cord) => parseInt(cord));
 
-		// Build function
-		constructBuilding(
-			user,
-			building, coordinates,
-		).then((response) => {
-			message.channel.send(`<@${message.author.id}>: ${response}`);
-		});
+    // Build function
+    constructBuilding(user, building, coordinates).then((response) => {
+      message.channel.send(`<@${message.author.id}>: ${response}`);
+    });
 
-		// Add !build help command to see the structures you can build
-	},
+    // Add !build help command to see the structures you can build
+  },
 };
--- game/destroy/destroy.js
+++ game/destroy/destroy.js
@@ -1,41 +1,50 @@
 const destroyHandler = async (user, arg) => {
-	const { response, message, building } = checkIfDestroyIsPossible(user, arg);
-	if(!response) return message;
+  const { response, message, building } = checkIfDestroyIsPossible(user, arg);
+  if (!response) return message;
 
-	const { name, level, position } = building;
+  const { name, level, position } = building;
 
-	// Remove the building from the database
-	try {
-		user.destroyBuilding(building);
-	}
-	catch {
-		console.error(`${user.account.username} was not able to destroy building ${name} (level: ${level}), at the position ${position[0]}.${position[1]}. The arg: "${arg}" was used`);
+  // Remove the building from the database
+  try {
+    user.destroyBuilding(building);
+  } catch {
+    console.error(
+      `${user.account.username} was not able to destroy building ${name} (level: ${level}), at the position ${position[0]}.${position[1]}. The arg: "${arg}" was used`
+    );
 
-		return "Something went wrong and you were not able to destroy the building successfully. Please report the bug and it will be fixed at the speed of light!";
-	}
+    return "Something went wrong and you were not able to destroy the building successfully. Please report the bug and it will be fixed at the speed of light!";
+  }
 
-	await user.save();
+  await user.save();
 
-	return `You successfully destroyed ${name} (level: ${level}) at the coordinates ${position[0]}.${position[1]}`;
+  return `You successfully destroyed ${name} (level: ${level}) at the coordinates ${position[0]}.${position[1]}`;
 };
 
 const checkIfDestroyIsPossible = (user, arg) => {
-	// Check if the user has the building with the specified coordinates or name
-	const building = arg.match(/\d+.|,\d+/) ? user.empire.find(b => b.position[0] === parseInt(arg.split(".")[0]) && b.position[1] === parseInt(arg.split(".")[1])) : user.empire.find(b => {
-		return b.name.toLowerCase() === arg;
-	});
+  // Check if the user has the building with the specified coordinates or name
+  const building = arg.match(/\d+.|,\d+/)
+    ? user.empire
+        .sort((a, b) => b.level - a.level)
+        .find(
+          (b) =>
+            b.position[0] === parseInt(arg.split(".")[0]) &&
+            b.position[1] === parseInt(arg.split(".")[1])
+        )
+    : user.empire.find((b) => {
+        return b.name.toLowerCase() === arg;
+      });
 
-	if(!building) {
-		return {
-			response: false,
-			message: `There are no buildings with the coordinates or name '${arg.toString()}' in your empire.`
-		};
-	}
+  if (!building) {
+    return {
+      response: false,
+      message: `There are no buildings with the coordinates or name '${arg.toString()}' in your empire.`,
+    };
+  }
 
-	return {
-		response: true,
-		building
-	};
+  return {
+    response: true,
+    building,
+  };
 };
 
 module.exports = { destroyHandler, checkIfDestroyIsPossible };
