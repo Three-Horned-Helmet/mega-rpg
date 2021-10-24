b43080309e490743034adadc46fdf85730dacf9e
Markus Heldrup <mah@teleplan.no>
2021-10-24 11:00:12 +0200

    removed test command to pprevent abuse

--- commands/test.js
+++ commands/test.js
@@ -9,31 +9,6 @@ module.exports = {
 	name: "test",
 	description: "dev tool",
 	async execute(message, args, user) {
-		const devs = ["SpinningSiri", "Ignore"];
-		if (!devs.includes(message.author.username)) {
-			return;
-		}
-
-		const t = await User.findOne({ "account.username":"SpinningSiri" });
-		const m = await User.findOne({ "account.username":"Ignore" });
-
-		const npc = getArmyTowerEnemies(Math.floor(Math.random() * 100));
-		const npc2 = getArmyTowerEnemies(Math.floor(Math.random() * 100));
-		const npc3 = getArmyTowerEnemies(Math.floor(Math.random() * 100));
-		const npc4 = getArmyTowerEnemies(Math.floor(Math.random() * 100));
-
-		const progress = {
-			combatRules:{
-				armyAllowed: false,
-				maxRounds: 3
-			},
-			teamGreen:[m, npc, npc2],
-			teamRed:[t, npc3, npc4],
-			embedInformation:{
-				minimal: true
-			}
-		};
-
-		return await createCombatRound(message, progress);
+		return
 	},
 };
